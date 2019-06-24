93d1c8b349c1f9e38e792ee0633f081e17bc446a
Wilfred Salmon <Wilfred.Salmon@softwire.com>
2019-06-24 16:33:42 +0100

    == to ===

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,22 +1,21 @@
 const start = 1;
 const end = 100;
 
-for (let i = start; i <= end; i++){
+for (let i = start; i <= end; i++) {
 
     let response = '';
 
-    if (i % 3 == 0) {
+    if (i % 3 === 0) {
         response += 'Fizz'
     }
 
-    if (i % 5 == 0) {
+    if (i % 5 === 0) {
         response += 'Buzz'
     }
 
-    if (response == '') {
+    if (response === '') {
         console.log(i)
     } else {
         console.log(response)
     }
-
 }
