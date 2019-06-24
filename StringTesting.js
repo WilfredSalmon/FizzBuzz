1dce375cee0702c5258eeba9d8a775998d60fdad
Wilfred Salmon <Wilfred.Salmon@softwire.com>
2019-06-24 17:14:30 +0100

    Added the 17 reversing rule

---
+++ StringTesting.js
@@ -0,0 +1,8 @@
+const string = 'abc';
+console.log(string.slice(0,1));
+console.log(string.slice(1,2));
+console.log(string.slice(1));
+
+const array = ['a','b','c'];
+array.splice(0,0,'NEW!');
+console.log(array);
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,19 +3,43 @@ const end = 100;
 
 for (let i = start; i <= end; i++) {
 
-    let response = '';
+    let response = [];
 
     if (i % 3 === 0) {
-        response += 'Fizz'
+        response.push('Fizz');
     }
 
     if (i % 5 === 0) {
-        response += 'Buzz'
+        response.push('Buzz');
     }
 
-    if (response === '') {
-        console.log(i)
+    if (i % 7 === 0) {
+        response.push('Bang');
+    }
+
+    if (i % 11 === 0) {
+        response = ['Bong'];
+    }
+
+    if (i % 13 === 0) {
+        const beginsWithB = response.map(value => value[0]==='B');
+        const index = beginsWithB.indexOf(true);
+
+        if (index === -1) {
+            response.push('Fezz');
+        }
+        else {
+            response.splice(index,0,'Fezz');
+        }
+    }
+
+    if (i % 17 === 0) {
+        response.reverse();
+    }
+
+    if (response.length === 0) {
+        console.log(i);
     } else {
-        console.log(response)
+        console.log(response.join(''));
     }
 }
