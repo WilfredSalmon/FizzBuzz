05510adb5c114b2d25cad287703430d78da345b1
Wilfred Salmon <Wilfred.Salmon@softwire.com>
2019-06-24 16:30:07 +0100

    Wrote the basic fizzbuzz program

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,22 @@
-console.log("foo");
+const start = 1;
+const end = 100;
+
+for (let i = start; i <= end; i++){
+
+    let response = '';
+
+    if (i % 3 == 0) {
+        response += 'Fizz'
+    }
+
+    if (i % 5 == 0) {
+        response += 'Buzz'
+    }
+
+    if (response == '') {
+        console.log(i)
+    } else {
+        console.log(response)
+    }
+
+}
